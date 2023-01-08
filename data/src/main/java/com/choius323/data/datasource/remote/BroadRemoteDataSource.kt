0984b3c5e54ff10f884e997664adc53e0b9eb517
ChoiUS <chldn323@gmail.com>
2023-01-08 19:47:14 +0900

    data 모듈에 방송 정보 DataSource 생성

---
+++ data/src/main/java/com/choius323/data/datasource/remote/BroadRemoteDataSource.kt
@@ -0,0 +1,11 @@
+package com.choius323.data.datasource.remote
+
+import androidx.paging.PagingData
+import com.choius323.domain.model.Broad
+import com.choius323.domain.model.Category
+import kotlinx.coroutines.flow.Flow
+
+interface BroadRemoteDataSource {
+    suspend fun getCategory(): Result<List<Category>>
+    fun getBroadList(category: Category): Flow<PagingData<Broad>>
+}
