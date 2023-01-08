51b556d402351a337aad6e6959112e0b56f65e0a
ChoiUS <chldn323@gmail.com>
2023-01-08 22:38:39 +0900

    BroadRemoteDataSource의 구현체 클래스 생성
    생방송 목록 가져오는 기능 구현

---
+++ data/src/main/java/com/choius323/data/datasource/remote/BroadRemoteDataSourceImpl.kt
@@ -0,0 +1,32 @@
+package com.choius323.data.datasource.remote
+
+import android.util.Log
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.choius323.data.BuildConfig
+import com.choius323.data.api.BroadService
+import com.choius323.domain.model.Broad
+import com.choius323.domain.model.Category
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class BroadRemoteDataSourceImpl @Inject constructor(
+    private val broadService: BroadService
+) : BroadRemoteDataSource {
+
+    override suspend fun getCategory(): Result<List<Category>> {
+        TODO()
+    }
+
+    override fun getBroadList(category: Category, pageSize: Int): Flow<PagingData<Broad>> {
+        return Pager(config = PagingConfig(pageSize = pageSize)) {
+            Log.d("BroadRemoteDataSourceImpl", "getBroadList: Pager")
+            BroadPagingSource(broadService, KEY_AFREECA, category, pageSize)
+        }.flow
+    }
+
+    companion object {
+        private const val KEY_AFREECA = BuildConfig.KEY_AFREECA
+    }
+}
