25a2622c55af05dc068f34693ef9a89dd6e28008
ChoiUS <chldn323@gmail.com>
2023-01-08 22:49:37 +0900

    domain, ui에서 사용할 생방송 클래스 추가

---
+++ domain/src/main/java/com/choius323/domain/model/Broad.kt
@@ -0,0 +1,18 @@
+package com.choius323.domain.model
+
+data class Broad(
+    val bps: String,
+    val categoryNum: String,
+    val grade: String,
+    val num: String,
+    val resolution: String,
+    val start: String,
+    val thumb: String,
+    val title: String,
+    val isPassword: String,
+    val profileImg: String,
+    val totalViewCnt: String,
+    val userId: String,
+    val userNick: String,
+    val visitBroadType: String,
+) : java.io.Serializable
